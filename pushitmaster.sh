c316f78b2af888487d5585ae65e97bcca7fa813b
Wilgert Velinga <wilgert@wilgert.nl>
2019-05-11 09:50:48 +0200

    turn in to git hook

--- pushitmaster.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-## ADD VARIABLES HERE ##
-_yourSoundFileAndPath="/Users/jeremykenedy/Desktop/pushit-jk-master/pushit8s.m4a"
-
-## ADD ALIAS'S HERE ##
-alias pushitmaster="git push origin master;afplay "$_yourSoundFileAndPath
